c015a07c86a5bbb8b97b219a33ca29e9eb86937c
Jared Puga Contreras <jaredpuga6@gmail.com>
2023-02-06 21:19:18 -0600

    database config

--- config/database.js
+++ config/database.js
@@ -1,20 +1,13 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "postgres",
-      settings: {
-        host: env("DATABASE_HOST"),
-        srv: env.bool("DATABASE_SRV"),
-        port: env.int("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-      },
-      options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE", "admin"),
-        ssl: env.bool("DATABASE_SSL", true),
-      },
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'dpg-cet166g2i3mj6pgqcco0-a.oregon-postgres.render.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'guitarla_l8rt'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'QzcjVG2ian5jEcDx4JsSMBgQ2bEtyHPX'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
